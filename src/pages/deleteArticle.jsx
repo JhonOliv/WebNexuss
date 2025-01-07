4b875475d6b9200cd5f75f18289b3df75f6c78cf
Jhonatan Silva <109522932+JhonOliv@users.noreply.github.com>
2025-01-07 10:15:23 -0300

    Update deleteArticle.jsx

--- src/pages/deleteArticle.jsx
+++ src/pages/deleteArticle.jsx
@@ -2,15 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
 import { useState } from "react";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDYqdxP_5hbArrg_R4_g7Iz3WMnV9wjCBE",
-    authDomain: "webnexuss.firebaseapp.com",
-    projectId: "webnexuss",
-    storageBucket: "webnexuss.firebasestorage.app",
-    messagingSenderId: "628801567818",
-    appId: "1:628801567818:web:76df575b1a112146a6bab3",
-    measurementId: "G-GEBPX15CBN",
-};
+
 
 // Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
