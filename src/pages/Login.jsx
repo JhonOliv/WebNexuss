f83f4c0053c44c4641625771739c4de0d8c2963f
Jhonatan Silva <109522932+JhonOliv@users.noreply.github.com>
2025-01-07 10:15:07 -0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,17 +4,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDYqdxP_5hbArrg_R4_g7Iz3WMnV9wjCBE",
-    authDomain: "webnexuss.firebaseapp.com",
-    projectId: "webnexuss",
-    storageBucket: "webnexuss.firebasestorage.app",
-    messagingSenderId: "628801567818",
-    appId: "1:628801567818:web:76df575b1a112146a6bab3",
-    measurementId: "G-GEBPX15CBN"
-};
-
-
 function Login() {
     const navigate = useNavigate(); // Hook para navegar programaticamente
 
